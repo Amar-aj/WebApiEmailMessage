2d54fa408daf394898c64cce755c2c1daecf2899
amar-tatwa <amarjyoti.mahanta@tatwa.info>
2025-02-20 15:24:52 +0530

    20 item 19 sec

--- WebApiEmailMessage/Services/EmailService.cs
+++ WebApiEmailMessage/Services/EmailService.cs
@@ -6,6 +6,7 @@ using System.Text.Json;
 using WebApiEmailMessage.Models;
 using Confluent.Kafka;
 using System.Net.Mail;
+using System;
 
 namespace WebApiEmailMessage.Services;
 
@@ -39,6 +40,7 @@ public class EmailService : IEmailService
 
         emailClient.Connect(_emailConfig.ImapServer, _emailConfig.ImapPort, true);
         emailClient.AuthenticationMechanisms.Remove("XOAUTH2");
+        //emailClient.Capabilities.HasFlag(ImapCapabilities.Pipelining);
         emailClient.AuthenticationMechanisms.Remove("NTLM");
         emailClient.Authenticate(username, password);
 
@@ -67,17 +69,19 @@ public class EmailService : IEmailService
 
     public async Task<List<EmailMessage>> GetEmails(int pageNumber = 1, int pageSize = 20)
     {
+        var messages = new List<EmailMessage>();
+        var lockObject = new object();
+
         using var emailClient = new ImapClient();
         var username = _emailConfig.ImapUsername;
         var password = _emailConfig.ImapPassword;
 
-        emailClient.Connect(_emailConfig.ImapServer, _emailConfig.ImapPort, true);
+        await emailClient.ConnectAsync(_emailConfig.ImapServer, _emailConfig.ImapPort, true);
         emailClient.AuthenticationMechanisms.Remove("XOAUTH2");
         emailClient.AuthenticationMechanisms.Remove("NTLM");
-        emailClient.Authenticate(username, password);
+        await emailClient.AuthenticateAsync(username, password);
 
-        List<EmailMessage> messages = new List<EmailMessage>();
-        var folders = emailClient.GetFolders(emailClient.PersonalNamespaces.First());
+        var folders = await emailClient.GetFoldersAsync(emailClient.PersonalNamespaces.First());
         if (folders.Any())
         {
             foreach (var folder in folders)
@@ -87,7 +91,7 @@ public class EmailService : IEmailService
                     await folder.OpenAsync(FolderAccess.ReadOnly);
 
                     int totalMessages = folder.Count;
-                    var summaries = await folder.FetchAsync(0, -1, MessageSummaryItems.Full | MessageSummaryItems.UniqueId);
+                    var summaries = await folder.FetchAsync(0, -1, MessageSummaryItems.UniqueId | MessageSummaryItems.Envelope);
                     var sortedSummaries = summaries
                         .Where(summary => summary.Date != null)
                         .OrderByDescending(summary => summary.Date)
@@ -98,74 +102,86 @@ public class EmailService : IEmailService
 
                     if (startIndex < sortedSummaries.Count)
                     {
+                        var tasks = new List<Task<EmailMessage>>();
                         for (int i = startIndex; i < endIndex; i++)
                         {
                             var summary = sortedSummaries[i];
-                            var item = await folder.GetMessageAsync(summary.UniqueId);
-
-                            var emailMessage = new EmailMessage
-                            {
-                                UniqueId = summary.UniqueId.Id,
-                                MessageId = item.MessageId,
-                                Subject = item.Subject,
-                                Body = string.IsNullOrEmpty(item.TextBody) ? item.HtmlBody : item.TextBody,
-                                Date = item.Date,
-                                IsReply = summary.IsReply,
-                                IsForward = item.Subject?.ToLowerInvariant().StartsWith("fw:") == true || item.Subject?.ToLowerInvariant().StartsWith("fwd:") == true,
-                                OriginalMessageId = item.Headers["In-Reply-To"]?.ToLowerInvariant(),
-                            };
-
-                            messages.Add(emailMessage);
-                            emailMessage.ToAddresses.AddRange(item.To.Select(x => (MailboxAddress)x).Select(x => new EmailAddress { Address = x.Address, Name = x.Name }));
-                            emailMessage.FromAddresses.AddRange(item.From.Select(x => (MailboxAddress)x).Select(x => new EmailAddress { Address = x.Address, Name = x.Name }));
-                            emailMessage.CcAddresses.AddRange(item.Cc.Select(x => (MailboxAddress)x).Select(x => new EmailAddress { Address = x.Address, Name = x.Name }));
-                            emailMessage.BccAddresses.AddRange(item.Bcc.Select(x => (MailboxAddress)x).Select(x => new EmailAddress { Address = x.Address, Name = x.Name }));
-
-                            foreach (var attachment in item.Attachments)
-                            {
-                                if (attachment is MimePart mimePart)
-                                {
-                                    var fileName = mimePart.FileName;
-                                    string filePath = Path.Combine("attachments", fileName);
-                                    Directory.CreateDirectory("attachments");
-                                    using (var stream = File.Create(filePath))
-                                    {
-                                        await mimePart.Content.DecodeToAsync(stream);
-                                    }
-
-                                    long size = mimePart.Content.Stream?.Length ?? 0;
-
-                                    emailMessage.Attachments.Add(new EmailAttachment
-                                    {
-                                        FileName = fileName,
-                                        FilePath = filePath,
-                                        Size = size,
-                                        ContentType = mimePart.ContentType.MimeType
-                                    });
-                                }
-                            }
-
-                            //var emailMessage = new EmailMessage
-                            //{
-                            //    MessageId = $"aa {i}",
-                            //    Subject = $"SS {i}",
-                            //    Body = $"Hii {i}",
-                            //    Date = DateTimeOffset.Now,
-                            //};
-
-                            // Publish to Kafka
-                            var topic = $"email/{username}";
-                            var message = JsonSerializer.Serialize(emailMessage);
-                            await _kafkaProducerService.ProduceAsync(topic, message);
+                            tasks.Add(GetEmailMessageAsync(folder, summary, lockObject));
                         }
+
+                        var emailMessages = await Task.WhenAll(tasks);
+                        messages.AddRange(emailMessages);
+
+                        // Publish to Kafka in batches
+                        var topic = $"email/{username}";
+                        var kafkaTasks = emailMessages.Select(emailMessage =>
+                        {
+                            var message = JsonSerializer.Serialize(emailMessage);
+                            return _kafkaProducerService.ProduceAsync(topic, message);
+                        });
+                        await Task.WhenAll(kafkaTasks);
                     }
 
-                    emailClient.Disconnect(true);
+                    await emailClient.DisconnectAsync(true);
                 }
             }
         }
         return messages;
     }
+
+    private async Task<EmailMessage> GetEmailMessageAsync(IMailFolder folder, IMessageSummary summary, object lockObject)
+    {
+        MimeMessage item;
+        lock (lockObject)
+        {
+            item = folder.GetMessage(summary.UniqueId);
+        }
+
+        var emailMessage = new EmailMessage
+        {
+            UniqueId = summary.UniqueId.Id,
+            MessageId = item.MessageId,
+            Subject = item.Subject,
+            Body = string.IsNullOrEmpty(item.TextBody) ? item.HtmlBody : item.TextBody,
+            Date = item.Date,
+            IsReply = summary.IsReply,
+            IsForward = item.Subject?.ToLowerInvariant().StartsWith("fw:") == true || item.Subject?.ToLowerInvariant().StartsWith("fwd:") == true,
+            OriginalMessageId = item.Headers["In-Reply-To"]?.ToLowerInvariant(),
+        };
+
+        emailMessage.ToAddresses.AddRange(item.To.Select(x => (MailboxAddress)x).Select(x => new EmailAddress { Address = x.Address, Name = x.Name }));
+        emailMessage.FromAddresses.AddRange(item.From.Select(x => (MailboxAddress)x).Select(x => new EmailAddress { Address = x.Address, Name = x.Name }));
+        emailMessage.CcAddresses.AddRange(item.Cc.Select(x => (MailboxAddress)x).Select(x => new EmailAddress { Address = x.Address, Name = x.Name }));
+        emailMessage.BccAddresses.AddRange(item.Bcc.Select(x => (MailboxAddress)x).Select(x => new EmailAddress { Address = x.Address, Name = x.Name }));
+
+        foreach (var attachment in item.Attachments)
+        {
+            if (attachment is MimePart mimePart)
+            {
+                var fileName = mimePart.FileName;
+                string filePath = Path.Combine("attachments", fileName);
+                Directory.CreateDirectory("attachments");
+                using (var stream = File.Create(filePath))
+                {
+                    await mimePart.Content.DecodeToAsync(stream);
+                }
+
+                long size = mimePart.Content.Stream?.Length ?? 0;
+
+                emailMessage.Attachments.Add(new EmailAttachment
+                {
+                    FileName = fileName,
+                    FilePath = filePath,
+                    Size = size,
+                    ContentType = mimePart.ContentType.MimeType
+                });
+            }
+        }
+
+        return emailMessage;
+    }
+
+
 
     public async Task<List<EmailMessage>> GetAllConsumers(string username, int pageNumber = 1, int pageSize = 20)
     {
